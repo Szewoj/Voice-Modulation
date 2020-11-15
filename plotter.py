f6f900d7df9f6e780f0e4d267ec6a12a4b7be8ec
MichalBan <01143758@pw.edu.pl>
2020-11-15 12:11:03 +0100

    lepsze wykresy

--- plotter.py
+++ plotter.py
@@ -13,6 +13,23 @@ if __name__ == "__main__":
 			break
 		times.append(float(buf))
 
+	plt.figure(1)
+	n, bins, patches = plt.hist(x=times, bins=7, color='#0504aa', alpha=0.7, rwidth=0.85)
+	plt.grid(axis='y', alpha=0.75)
+	plt.xlabel('Opoznienie')
+	plt.ylabel('Wystapienia')
+	plt.title('Histogram')
+	plt.ylim(ymax=n.max() + 5)
+
+	plt.figure(2)
 	plt.plot(times)
-	plt.show()
+	plt.grid(axis='y', alpha=0.75)
+	plt.xlabel('Czas')
+	plt.ylabel('Opoznienie')
+	plt.title('Przebieg czasowy')
+	plt.ylim(ymax=max(times) = 0.01)
+
 	print("ploting successfull")
+	plt.show()
+	print("closing plots")
+	exit(0)
