dd5a43b0b2ea5212b4d2a49df9147462e3301221
berochel <zabuski.jaroslaw@gmail.com>
2020-12-21 12:36:57 +0100

    Update process A

--- src/capture.c
+++ src/capture.c
@@ -119,6 +119,7 @@ int main(void)
         fid = fopen("samp/raw.raw", "wb");
         if( fid != NULL )
         {
+            /*https://stackoverflow.com/questions/10192903/time-in-milliseconds-in-c*/
             gettimeofday(&start, NULL);
             fwrite( start, sizeof(struct timeval), 1, fid);
             fwrite( samplesRecorded, NUM_CHANNELS * sizeof(SAMPLE), amountOfFrames, fid );
